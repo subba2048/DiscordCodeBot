a5faa05c9c4e954b03c88948cd6f83bdad965898
Subbalakshmi Mandapaka <subbalakshmi.mandapaka01@utrgv.edu>
2024-11-11 12:32:15 -0600

    Add meme - api

--- bot.py
+++ bot.py
@@ -1,10 +1,24 @@
+import requests
+import json
+
+def get_meme():
+  response = requests.get('https://meme-api.com/gimme')
+  json_data = json.loads(response.text)
+  return json_data['url']
+
 import discord
 import os
 from dotenv import load_dotenv
 
 class MyClient(discord.Client):
-  async def on_ready(self):
-    print('Logged on as {0}!'.format(self.user))
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$meme'):
+            await message.channel.send(get_meme())
 
 intents = discord.Intents.default()
 intents.message_content = True
